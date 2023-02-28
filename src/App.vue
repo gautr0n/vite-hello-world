ec2b1dc671d54bc7809d00e35d157415f6b4208a
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-02-28 18:04:32 +0100

    printing title

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+// 1° importazione
+  import appTitle from './components/appTitle.vue'
+
+// 2° registrare componente
+  export default{
+    components: {
+      appTitle
+    }
+}
 </script>
 
+<!-- 3° utilizzare la componente -->
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1><appTitle></appTitle></h1>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
 </style>
---
+++ src/components/appTitle.vue
@@ -0,0 +1,15 @@
+<script>
+export default{
+    data() {
+        return{
+            msg : 'La mia prima app con Vite!'
+        }
+    },
+}
+</script>
+
+<template>
+    <div>
+        {{ msg }}
+    </div>
+</template>
